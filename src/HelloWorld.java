705b0343e907cd452a67a1121e47603a33698a50
Yasemin <ysmnyolcu@gmail.com>
2022-04-04 00:14:45 +0200

    work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 // VCS: Version control system. 'VSC' option will turn into 'Git' from this point
 
 // Doing some work here (  you can type anything you want)
-// Doing awesome work 2 !
+// Doing awesome work 2
 
 }
